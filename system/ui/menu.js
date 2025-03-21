2f09d46e11c2dfbeccb6474bb130e67c81ff0fe5
Zack Sigmund <zack@zacksigmund.com>
2025-03-20 23:39:03 -0400

    Trap focus in menu, focus first element on open, and use arrow keys to move items

--- system/ui/menu.js
+++ system/ui/menu.js
@@ -4,26 +4,45 @@ import { UnstyledButton } from "./unstyled-button.js";
 export const Menu = (menuItems) => {
     let menuOpen = false;
 
-    // TODO: focus trap
-    const menu = Element(
-        "ul",
-        { class: "sf-menu", role: "menu" },
-        ...Object.entries(menuItems).map(([name, callback]) =>
-            Element("li", { role: "menuitem" }, UnstyledButton({}, callback, name))
-        )
+    const items = Object.entries(menuItems).map(([name, callback]) =>
+        Element("li", { role: "menuitem" }, UnstyledButton({}, callback, name))
     );
 
-    const toggleMenu = (e) => {
+    const menu = Element("ul", { class: "sf-menu", role: "menu" }, ...items);
+
+    const trapFocus = (event) => {
+        if (event.key === "Tab") {
+            if (!event.shiftKey && event.target === items[items.length - 1].children[0]) {
+                items[0].children[0].focus();
+                event.preventDefault();
+            } else if (event.shiftKey && event.target === items[0].children[0]) {
+                items[items.length - 1].children[0].focus();
+                event.preventDefault();
+            }
+        } else if (event.key === "ArrowDown") {
+            event.target.parentElement.nextElementSibling?.children?.[0]?.focus();
+        } else if (event.key === "ArrowUp") {
+            event.target.parentElement.previousElementSibling?.children?.[0]?.focus();
+        } else if (event.key === "Escape") {
+            offClick();
+        }
+    };
+
+    menu.addEventListener("keydown", trapFocus);
+
+    const offClick = () => {
+        if (menuOpen) {
+            menu.style.display = "none";
+            document.body.removeEventListener("click", offClick);
+            menuOpen = false;
+        }
+    };
+
+    const toggleMenu = () => {
         if (!menuOpen) {
             menu.style.display = "flex";
             menuOpen = true;
-            const offClick = () => {
-                if (menuOpen) {
-                    menu.style.display = "none";
-                    document.body.removeEventListener("click", offClick);
-                    menuOpen = false;
-                }
-            };
+            menu.firstElementChild.firstElementChild.focus();
             // was propagating to the offClick listener on the same frame otherwise
             requestAnimationFrame(() => {
                 document.body.addEventListener("click", offClick);
