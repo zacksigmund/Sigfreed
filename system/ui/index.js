3c0aa99c26ce6e5910e67f8f6b1b82cab90c7b57
Zack Sigmund <zack@zacksigmund.com>
2025-03-25 21:22:24 -0400

    Add RadioGroup component

--- system/ui/index.js
+++ system/ui/index.js
@@ -2,9 +2,9 @@ import { Button } from "./button.js";
 import { Checkbox } from "./checkbox.js";
 import { CrossoutCheckbox } from "./crossout-checkbox.js";
 import { Element } from "./element.js";
-import { RadioButton } from "./radio-button.js";
+import { RadioGroup } from "./radio-button.js";
 import { Select } from "./select.js";
 import { Textbox } from "./textbox.js";
 import { Window } from "./window.js";
 
-export { Button, Checkbox, CrossoutCheckbox, Element, RadioButton, Select, Textbox, Window };
+export { Button, Checkbox, CrossoutCheckbox, Element, RadioGroup, Select, Textbox, Window };
--- system/ui/radio-button.js
+++ system/ui/radio-button.js
@@ -11,3 +11,21 @@ export const RadioButton = (attrs, onChange, ...children) => {
     const label = Element("label", { class: "sf-radio" }, input, Element("span", {}, ...children));
     return label;
 };
+
+export const RadioGroup = (legend, name, entries, checkedValue, onChange) => {
+    const change = (event) => {
+        onChange?.(event.target.value);
+    };
+    return Element(
+        "fieldset",
+        { class: "sf-radiogroup" },
+        Element("legend", {}, legend),
+        Element(
+            "div",
+            {},
+            ...Object.entries(entries).map(([value, label]) =>
+                RadioButton({ name, value, checked: checkedValue === value }, change, label)
+            )
+        )
+    );
+};
