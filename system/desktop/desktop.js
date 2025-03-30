f3ceb651d237bd4c595c308b059e4c056c0c9890
Zack Sigmund <zack@zacksigmund.com>
2025-03-30 18:02:39 -0400

    Hide dock scrollbar and provide gradient scrollable hint

--- system/desktop/desktop.js
+++ system/desktop/desktop.js
@@ -10,24 +10,37 @@ import { DockIcon } from "./dock-icon.js";
 import { StatusBar } from "./statusbar.js";
 
 export const Desktop = () => {
+    const dockScroll = Element(
+        "div",
+        {},
+        DockIcon(Todo),
+        DockIcon(Calendar),
+        DockIcon(Weather),
+        DockIcon(Calculator),
+        DockIcon(Solitaire),
+        DockIcon(FreeCell),
+        DockIcon(Spider)
+    );
+    const updateGradient = () => {
+        if (dockScroll.scrollLeft <= 8) {
+            dockScroll.classList.remove("gradientStart");
+        } else {
+            dockScroll.classList.add("gradientStart");
+        }
+
+        if (dockScroll.scrollLeft + dockScroll.clientWidth >= dockScroll.scrollWidth - 8) {
+            dockScroll.classList.remove("gradientEnd");
+        } else {
+            dockScroll.classList.add("gradientEnd");
+        }
+    };
+    dockScroll.addEventListener("scroll", updateGradient);
+    setTimeout(updateGradient, 15);
+
     return Element(
         "div",
         { class: "sf-desktop" },
         StatusBar(),
-        Element(
-            "div",
-            { class: "sf-dock" },
-            Element(
-                "div",
-                {},
-                DockIcon(Todo),
-                DockIcon(Calendar),
-                DockIcon(Weather),
-                DockIcon(Calculator),
-                DockIcon(Solitaire),
-                DockIcon(FreeCell),
-                DockIcon(Spider)
-            )
-        )
+        Element("div", { class: "sf-dock" }, dockScroll)
     );
 };
