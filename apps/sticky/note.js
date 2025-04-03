b5b92f278e972080f32005a61c5d40c906e00f89
Zack Sigmund <zack@zacksigmund.com>
2025-04-02 21:22:30 -0400

    Multi notes work!

---
+++ apps/sticky/note.js
@@ -0,0 +1,97 @@
+import { Element, UnstyledButton } from "../../system/ui/index.js";
+
+export class Note {
+    constructor(id, newSticky) {
+        this.moving = false;
+        this.id = id;
+
+        const dragger = Element("div", { class: "dragger" });
+
+        const note = Element(
+            "textarea",
+            {
+                class: "note",
+                "aria-label": "Note text",
+                placeholder: "Write something...",
+            },
+            localStorage.getItem(`Sticky.${id}`) ?? ""
+        );
+
+        note.addEventListener("blur", () => {
+            localStorage.setItem(`Sticky.${id}`, note.value);
+        });
+
+        const deleteButton = UnstyledButton({ "aria-label": "Delete sticky note" }, null, "×");
+
+        const windowEl = Element(
+            "dialog",
+            { class: "sf-sticky" },
+            Element(
+                "div",
+                { class: "menubar" },
+                UnstyledButton({ "aria-label": "New sticky note" }, newSticky, "+"),
+                dragger,
+                deleteButton
+            ),
+            note
+        );
+
+        deleteButton.addEventListener("click", () => {
+            localStorage.removeItem(`Sticky.${id}`);
+            localStorage.removeItem(`window.Sticky.${this.id}`);
+            windowEl.parentElement.removeChild(windowEl);
+        });
+
+        // window movement
+        dragger.addEventListener("mousedown", () => {
+            this.moving = true;
+            dragger.classList.add("dragging");
+        });
+        window.addEventListener("mousemove", (event) => {
+            if (!this.moving) return;
+
+            windowEl.style.top = `${windowEl.offsetTop + event.movementY}px`;
+            if (windowEl.offsetTop < 0) {
+                windowEl.style.top = 0;
+            } else if (windowEl.offsetTop + windowEl.clientHeight > window.innerHeight) {
+                windowEl.style.top = `${window.innerHeight - windowEl.clientHeight}px`;
+            }
+
+            windowEl.style.left = `${windowEl.offsetLeft + event.movementX}px`;
+            if (windowEl.offsetLeft < 0) {
+                windowEl.style.left = 0;
+            } else if (windowEl.offsetLeft + windowEl.clientWidth > window.innerWidth) {
+                windowEl.style.left = `${window.innerWidth - windowEl.clientWidth}px`;
+            }
+        });
+        window.addEventListener("mouseup", (event) => {
+            if (!this.moving) return;
+            windowEl.style.top = `${windowEl.offsetTop + event.movementY}px`;
+            windowEl.style.left = `${windowEl.offsetLeft + event.movementX}px`;
+            this.moving = false;
+            dragger.classList.remove("dragging");
+            this.saveLocation(windowEl.offsetTop, windowEl.offsetLeft);
+        });
+
+        const { top, left } = this.loadLocation();
+        windowEl.style.top = `${top}px`;
+        windowEl.style.left = `${left}px`;
+
+        this.windowEl = windowEl;
+    }
+
+    saveLocation = (top, left) => {
+        localStorage.setItem(`window.Sticky.${this.id}`, JSON.stringify({ top, left }));
+    };
+
+    loadLocation = () => {
+        const locationValue = JSON.parse(localStorage.getItem(`window.Sticky.${this.id}`));
+        if (locationValue === null) {
+            return {
+                top: 100,
+                left: 200,
+            };
+        }
+        return locationValue;
+    };
+}
--- apps/sticky/sticky.js
+++ apps/sticky/sticky.js
@@ -1,95 +1,25 @@
-import { Element, UnstyledButton } from "../../system/ui/index.js";
+import { Element } from "../../system/ui/index.js";
+import { Note } from "./note.js";
 
 export class Sticky {
     static name = "Sticky";
     static icon = "apps/sticky/sticky.app.png";
     constructor() {
-        this.moving = false;
-
+        let stickyCount = parseInt(localStorage.getItem(`${Sticky.name}.count`) ?? 1);
         const newSticky = () => {
-            // new sticky
+            const note = new Note(stickyCount, newSticky).windowEl;
+            this.windowEl.appendChild(note);
+            note.show();
+            localStorage.setItem(`${Sticky.name}.count`, ++stickyCount);
         };
-        const deleteSticky = () => window.windowManager.close(Sticky.name);
+        const notes = Array.from(Array(stickyCount))
+            .map((_, i) =>
+                localStorage.getItem(`${Sticky.name}.${i}`) ? new Note(i, newSticky).windowEl : null
+            )
+            .filter((x) => x);
 
-        const dragger = Element("div", { class: "dragger" });
-
-        const note = Element(
-            "textarea",
-            {
-                class: "note",
-                "aria-label": "Note text",
-                placeholder: "Write something...",
-            },
-            localStorage.getItem(Sticky.name) ?? ""
-        );
-
-        note.addEventListener("blur", () => {
-            localStorage.setItem(Sticky.name, note.value);
-        });
-
-        const windowEl = Element(
-            "dialog",
-            { class: "sf-sticky" },
-            Element(
-                "div",
-                { class: "menubar" },
-                UnstyledButton({ "aria-label": "New sticky note" }, newSticky, "+"),
-                dragger,
-                UnstyledButton({ "aria-label": "Delete sticky note" }, deleteSticky, "×")
-            ),
-            note
-        );
-
-        // window movement
-        dragger.addEventListener("mousedown", () => {
-            this.moving = true;
-            dragger.classList.add("dragging");
-        });
-        window.addEventListener("mousemove", (event) => {
-            if (!this.moving) return;
-
-            windowEl.style.top = `${windowEl.offsetTop + event.movementY}px`;
-            if (windowEl.offsetTop < 0) {
-                windowEl.style.top = 0;
-            } else if (windowEl.offsetTop + windowEl.clientHeight > window.innerHeight) {
-                windowEl.style.top = `${window.innerHeight - windowEl.clientHeight}px`;
-            }
-
-            windowEl.style.left = `${windowEl.offsetLeft + event.movementX}px`;
-            if (windowEl.offsetLeft < 0) {
-                windowEl.style.left = 0;
-            } else if (windowEl.offsetLeft + windowEl.clientWidth > window.innerWidth) {
-                windowEl.style.left = `${window.innerWidth - windowEl.clientWidth}px`;
-            }
-        });
-        window.addEventListener("mouseup", (event) => {
-            if (!this.moving) return;
-            windowEl.style.top = `${windowEl.offsetTop + event.movementY}px`;
-            windowEl.style.left = `${windowEl.offsetLeft + event.movementX}px`;
-            this.moving = false;
-            dragger.classList.remove("dragging");
-            this.saveLocation(windowEl.offsetTop, windowEl.offsetLeft);
-        });
-
-        const { top, left } = this.loadLocation();
-        windowEl.style.top = `${top}px`;
-        windowEl.style.left = `${left}px`;
-
-        this.windowEl = windowEl;
+        this.windowEl = Element("div", {}, ...notes);
+        this.windowEl.show = () => notes.map((note) => note.show());
+        if (notes.length === 0) newSticky();
     }
-
-    saveLocation = (top, left) => {
-        localStorage.setItem(`window.${Sticky.name}`, JSON.stringify({ top, left }));
-    };
-
-    loadLocation = () => {
-        const locationValue = JSON.parse(localStorage.getItem(`window.${Sticky.name}`));
-        if (locationValue === null) {
-            return {
-                top: 100,
-                left: 200,
-            };
-        }
-        return locationValue;
-    };
 }
