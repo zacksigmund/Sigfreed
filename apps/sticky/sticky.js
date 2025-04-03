84ea951ccb39006cdb4ebae4cd3cc5057a0e85a5
Zack Sigmund <zack@zacksigmund.com>
2025-04-02 20:21:53 -0400

    Make sticky note mostly functional as one

--- apps/sticky/sticky.js
+++ apps/sticky/sticky.js
@@ -1,9 +1,95 @@
-import { Element } from "../../system/ui/index.js";
+import { Element, UnstyledButton } from "../../system/ui/index.js";
 
 export class Sticky {
     static name = "Sticky";
     static icon = "apps/sticky/sticky.app.png";
     constructor() {
-        this.windowEl = Element("dialog", { class: "sf-sticky" }, "Text");
+        this.moving = false;
+
+        const newSticky = () => {
+            // new sticky
+        };
+        const deleteSticky = () => window.windowManager.close(Sticky.name);
+
+        const dragger = Element("div", { class: "dragger" });
+
+        const note = Element(
+            "textarea",
+            {
+                class: "note",
+                "aria-label": "Note text",
+                placeholder: "Write something...",
+            },
+            localStorage.getItem(Sticky.name) ?? ""
+        );
+
+        note.addEventListener("blur", () => {
+            localStorage.setItem(Sticky.name, note.value);
+        });
+
+        const windowEl = Element(
+            "dialog",
+            { class: "sf-sticky" },
+            Element(
+                "div",
+                { class: "menubar" },
+                UnstyledButton({ "aria-label": "New sticky note" }, newSticky, "+"),
+                dragger,
+                UnstyledButton({ "aria-label": "Delete sticky note" }, deleteSticky, "×")
+            ),
+            note
+        );
+
+        // window movement
+        dragger.addEventListener("mousedown", () => {
+            this.moving = true;
+            dragger.classList.add("dragging");
+        });
+        window.addEventListener("mousemove", (event) => {
+            if (!this.moving) return;
+
+            windowEl.style.top = `${windowEl.offsetTop + event.movementY}px`;
+            if (windowEl.offsetTop < 0) {
+                windowEl.style.top = 0;
+            } else if (windowEl.offsetTop + windowEl.clientHeight > window.innerHeight) {
+                windowEl.style.top = `${window.innerHeight - windowEl.clientHeight}px`;
+            }
+
+            windowEl.style.left = `${windowEl.offsetLeft + event.movementX}px`;
+            if (windowEl.offsetLeft < 0) {
+                windowEl.style.left = 0;
+            } else if (windowEl.offsetLeft + windowEl.clientWidth > window.innerWidth) {
+                windowEl.style.left = `${window.innerWidth - windowEl.clientWidth}px`;
+            }
+        });
+        window.addEventListener("mouseup", (event) => {
+            if (!this.moving) return;
+            windowEl.style.top = `${windowEl.offsetTop + event.movementY}px`;
+            windowEl.style.left = `${windowEl.offsetLeft + event.movementX}px`;
+            this.moving = false;
+            dragger.classList.remove("dragging");
+            this.saveLocation(windowEl.offsetTop, windowEl.offsetLeft);
+        });
+
+        const { top, left } = this.loadLocation();
+        windowEl.style.top = `${top}px`;
+        windowEl.style.left = `${left}px`;
+
+        this.windowEl = windowEl;
     }
+
+    saveLocation = (top, left) => {
+        localStorage.setItem(`window.${Sticky.name}`, JSON.stringify({ top, left }));
+    };
+
+    loadLocation = () => {
+        const locationValue = JSON.parse(localStorage.getItem(`window.${Sticky.name}`));
+        if (locationValue === null) {
+            return {
+                top: 100,
+                left: 200,
+            };
+        }
+        return locationValue;
+    };
 }
--- system/ui/index.js
+++ system/ui/index.js
@@ -5,6 +5,17 @@ import { Element } from "./element.js";
 import { RadioGroup } from "./radio-button.js";
 import { Select } from "./select.js";
 import { Textbox } from "./textbox.js";
+import { UnstyledButton } from "./unstyled-button.js";
 import { Window } from "./window.js";
 
-export { Button, Checkbox, CrossoutCheckbox, Element, RadioGroup, Select, Textbox, Window };
+export {
+    Button,
+    Checkbox,
+    CrossoutCheckbox,
+    Element,
+    RadioGroup,
+    Select,
+    Textbox,
+    UnstyledButton,
+    Window,
+};
