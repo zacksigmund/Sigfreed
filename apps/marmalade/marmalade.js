fc3e88e1cb3d84ab37fb39bb05a66a720896bf17
Zack Sigmund <zack@zacksigmund.com>
2025-10-23 23:58:04 -0400

    Type text and Tab/Enter

--- apps/marmalade/marmalade.js
+++ apps/marmalade/marmalade.js
@@ -6,37 +6,74 @@ export class Marmalade {
     static icon = "apps/calculator/calculator.app.png";
     static about = "What a delicious spread!";
 
-    static numCols = 3;
-    static numRows = 5;
+    static numCols = 5;
+    static numRows = 10;
 
     constructor() {
-        const formulaBar = Element(
-            "div",
-            { class: "formula-bar" },
-            Element("span", {}, "f(x)"),
-            Textbox()
-        );
+        this.formulaBox = Textbox({ class: "formula-input" });
+        this.formulaInput = this.formulaBox.querySelector("input");
+        this.formulaBox.addEventListener("keydown", (event) => {
+            if (event.key === "Tab") {
+                this.executeFormula(event);
+            }
+        });
         const table = Element(
             "table",
             { class: "table" },
-            ...range(Marmalade.numRows).map((i) =>
-                Element(
-                    "tr",
-                    {},
-                    ...range(Marmalade.numCols).map(() =>
-                        Element(
-                            i === 0 ? "th" : "td",
-                            {},
-                            UnstyledButton({}, () => {}, "")
+            Element(
+                "thead",
+                {},
+                Element("tr", {}, ..." ABCDE".split("").map((char) => Element("th", {}, char)))
+            ),
+            Element(
+                "tbody",
+                {},
+                ...range(Marmalade.numRows).map((i) =>
+                    Element(
+                        "tr",
+                        {},
+                        Element("th", {}, i + 1),
+                        ...range(Marmalade.numCols).map(() =>
+                            Element("td", {}, UnstyledButton({}, this.selectCell, ""))
                         )
                     )
                 )
             )
         );
+        const form = Element(
+            "form",
+            { class: "formula-bar", onSubmit: this.executeFormula },
+            Element("span", { class: "fx" }, "f(x)"),
+            this.formulaBox
+        );
+        form.addEventListener("submit", this.executeFormula);
         this.windowEl = Window(
             Marmalade.name,
             [["About", "/", () => alert(Marmalade.about)]],
-            Element("div", { class: "sf-marmalade" }, formulaBar, table)
+            Element("div", { class: "sf-marmalade" }, form, table)
         );
     }
+
+    selectCell = (event) => {
+        this.selectedCell?.classList.remove("selected");
+        this.selectedCell = event.target;
+        this.selectedCell.classList.add("selected");
+        this.formulaInput.value = this.selectedCell.textContent;
+        this.formulaInput.focus();
+    };
+
+    executeFormula = (event) => {
+        event.preventDefault();
+        if (this.selectedCell) {
+            this.selectedCell.textContent = this.formulaInput.value;
+            this.formulaInput.value = "";
+            if (event.type === "keydown") {
+                this.selectedCell.parentElement.nextElementSibling?.querySelector("button").click();
+            } else {
+                const row = this.selectedCell.closest("tr");
+                const cellIndex = Array.from(row.cells).indexOf(this.selectedCell.parentElement);
+                row.nextElementSibling?.cells[cellIndex]?.querySelector("button").click();
+            }
+        }
+    };
 }
